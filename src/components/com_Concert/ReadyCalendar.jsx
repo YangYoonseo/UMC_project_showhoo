1fa28f012093ab30d3dedbf31a34047abe9e23df
YangYoonseo <yys0847@naver.com>
2024-08-06 18:01:32 +0900

    update: 캘린더 수정

--- src/components/com_Concert/ReadyCalendar.jsx
+++ src/components/com_Concert/ReadyCalendar.jsx
@@ -1,10 +1,8 @@
 import Calendar from "react-calendar";
 import "../../styles/yoonseo/ReadyCalendar.css";
 import "react-calendar/dist/Calendar.css";
-import { useState } from "react";
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { ProfileContext } from "../../App";
-
 import PerformerCalendar from "./PerformerCalendar";
 
 const ReadyCalendar = () => {
@@ -32,6 +30,36 @@ const ReadyCalendar = () => {
     setCompleted(true);
   };
 
+  const getTileContent = ({ date, view }) => {
+    if (view === "month") {
+      if (date.getDate() === 1) {
+        return (
+          <div className="event event_show" onClick={handleEventShow}>
+            공연 완료
+          </div>
+        );
+      } else if (date.getDate() === 13) {
+        return (
+          <div
+            className="event event_application"
+            onClick={handleEventApplication}
+          >
+            대관 신청
+          </div>
+        );
+      } else if (date.getDate() === 23) {
+        return (
+          <div className="event event_completed" onClick={handleEventCompleted}>
+            대관 완료
+          </div>
+        );
+      } else if (date.getDate() === 16 || date.getDate() === 28) {
+        return <div className="event event_application">대관 신청</div>;
+      }
+    }
+    return null;
+  };
+
   return (
     <div className="ReadyCalendar">
       <Calendar
@@ -40,40 +68,9 @@ const ReadyCalendar = () => {
         formatDay={(locale, date) => date.getDate().toString()} // 일 제거 숫자만 보이게
         prev2Label={null}
         next2Label={null}
-        calendarType="gregory" //시작 요일 일요일로 변경
-        showNeighboringMonth={false} //이웃달 안 보여줌
-        tileContent={({ date, view }) => {
-          if (view === "month") {
-            // 추후에 수정해야함. 현재는 날짜별로 되어있음
-            if (date.getDate() === 1) {
-              return (
-                <div className="event event_show" onClick={handleEventShow}>
-                  공연 완료
-                </div>
-              );
-            } else if (date.getDate() === 13) {
-              return (
-                <div
-                  className="event event_application"
-                  onClick={handleEventApplication}
-                >
-                  대관 신청
-                </div>
-              );
-            } else if (date.getDate() === 23) {
-              return (
-                <div
-                  className="event event_completed"
-                  onClick={handleEventCompleted}
-                >
-                  대관 완료
-                </div>
-              );
-            } else if (date.getDate() === 16 || date.getDate() === 28) {
-              return <div className="event event_application">대관 신청</div>;
-            }
-          }
-        }}
+        calendarType="gregory" // 시작 요일 일요일로 변경
+        showNeighboringMonth={false} // 이웃달 안 보여줌
+        tileContent={getTileContent}
       />
 
       {/* 공연완료 */}
