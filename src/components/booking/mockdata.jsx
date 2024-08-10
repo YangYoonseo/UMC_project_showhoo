4ab98cc80149d89dfaa3be1f2f1492c897fb6d41
ydjwls <xqhfkfpalx@naver.com>
2024-08-11 00:28:24 +0900

    feat: 관람자 예매 - 세부사항 구현 완료

--- src/components/booking/mockdata.jsx
+++ src/components/booking/mockdata.jsx
@@ -3,151 +3,163 @@ import poster2 from "../../assets/img_Booking/Booking/poster2.svg";
 import poster3 from "../../assets/img_Booking/Booking/poster3.svg";
 import poster4 from "../../assets/img_Booking/Booking/poster4.svg";
 
-export default Mockdata=[
+const Mockdata = [
     {
         id: 0,
         poster: poster1,
+        price: 4000,
         name: "[최장순&미지니 콘서트] 파란약방.exe",
         date: new Date('2024-08-18T14:00:00'),
         like: false,
+        amount: 70,
         place: '합정 드림홀',
         runningtime: 120,
         host: '고스락',
         cancel: new Date('2024-08-17T23:30:00'),
         concert_inf: `
-            3월 7일, 활동 기수들의 멋진 공연에 이어, <br><br>
+            3월 7일, 활동 기수들의 멋진 공연에 이어, <br>
             이번에는 3월 8일 19시, 합정 드림홀에서 고스락 선배님들의 공연이 막을 올립니다!<br><br>
             총 다섯 팀이 열심히 준비한 무대를 선보일 예정이니, 3월 8일에 합정 드림홀에서 만나요!<br><br>
-            <strong>세트리스트</strong><br>
+            <strong>· 세트리스트</strong><br>
             잔나비-투게더! Fall Out Boy-Centuries<br>
             실리카겔 - Tik Tak Tok My Chemical Romance- Welcome to the Black Parade<br><br>
-            <strong>공연 문의 안내</strong><br>
+            <strong>· 공연 문의 안내</strong><br>
             공연에 대한 모든 문의는 <a href="https://link.to/kakao_channel" target="_blank">고스락 카카오톡 채널</a>로 남겨주세요!<br>
         `,
         refund_inf: `
-            <strong>환불 안내</strong><br>
+            <strong>· 환불 안내</strong><br>
             공연이라는 상품의 특성상 공연이 종료되면 상품 가치가 소멸합니다. 따라서 공연 시작 이후에는 환불이 어렵습니다. 공연 시작 시간은 각 공연 상세 페이지에서 확인하실 수 있습니다. 취소 수수료는 따로 없습니다.<br><br>
-            승인, 선착순 방식의 경우 <span style="color: #FF0000;">공연 시작 전까지</span> 마이페이지 예매내역에서 환불 가능합니다.<br><br>
-            <strong>공연 입장 안내</strong><br>
+            승인, 선착순 방식의 경우 <span style="color: #F01569;">공연 시작 전까지</span> 마이페이지 예매내역에서 환불 가능합니다.<br><br>
+            <strong>· 공연 입장 안내</strong><br>
             공연 입장 전에 "이름"과 "전화번호"를 말씀하시면, 공연자가 예매자 확인 후 입장이 가능합니다.<br>
         `
     },
     {
         id: 1,
         poster: poster2,
+        price: 4000,
         name: "2024 시나 쓰는 앨리스 소극장 콘서트 [핑]",
         date: new Date('2024-08-18T14:00:00'),
         like: false,
         place: '합정 드림홀',
+        amount: 70,
         runningtime: 120,
         host: '고스락',
         cancel: new Date('2024-08-17T23:30:00'),
         concert_inf: `
-            3월 7일, 활동 기수들의 멋진 공연에 이어, <br><br>
+            3월 7일, 활동 기수들의 멋진 공연에 이어, <br>
             이번에는 3월 8일 19시, 합정 드림홀에서 고스락 선배님들의 공연이 막을 올립니다!<br><br>
             총 다섯 팀이 열심히 준비한 무대를 선보일 예정이니, 3월 8일에 합정 드림홀에서 만나요!<br><br>
-            <strong>세트리스트</strong><br>
+            <strong>· 세트리스트</strong><br>
             잔나비-투게더! Fall Out Boy-Centuries<br>
             실리카겔 - Tik Tak Tok My Chemical Romance- Welcome to the Black Parade<br><br>
-            <strong>공연 문의 안내</strong><br>
+            <strong>· 공연 문의 안내</strong><br>
             공연에 대한 모든 문의는 <a href="https://link.to/kakao_channel" target="_blank">고스락 카카오톡 채널</a>로 남겨주세요!<br>
         `,
         refund_inf: `
-            <strong>환불 안내</strong><br>
+            <strong>· 환불 안내</strong><br>
             공연이라는 상품의 특성상 공연이 종료되면 상품 가치가 소멸합니다. 따라서 공연 시작 이후에는 환불이 어렵습니다. 공연 시작 시간은 각 공연 상세 페이지에서 확인하실 수 있습니다. 취소 수수료는 따로 없습니다.<br><br>
-            승인, 선착순 방식의 경우 <span style="color: #FF0000;">공연 시작 전까지</span> 마이페이지 예매내역에서 환불 가능합니다.<br><br>
-            <strong>공연 입장 안내</strong><br>
+            승인, 선착순 방식의 경우 <span style="color: #F01569;">공연 시작 전까지</span> 마이페이지 예매내역에서 환불 가능합니다.<br><br>
+            <strong>· 공연 입장 안내</strong><br>
             공연 입장 전에 "이름"과 "전화번호"를 말씀하시면, 공연자가 예매자 확인 후 입장이 가능합니다.<br>
         `
     },
     {
         id: 2,
         poster: poster3,
+        price: 4000,
         name: "[홍익대학교 소리얼] SO:RE-ALIVE",
         date: new Date('2024-08-18T14:00:00'),
         like: false,
+        amount: 70,
         place: '합정 드림홀',
         runningtime: 120,
         host: '고스락',
         cancel: new Date('2024-08-17T23:30:00'),
         concert_inf: `
-            3월 7일, 활동 기수들의 멋진 공연에 이어, <br><br>
+            3월 7일, 활동 기수들의 멋진 공연에 이어, <br>
             이번에는 3월 8일 19시, 합정 드림홀에서 고스락 선배님들의 공연이 막을 올립니다!<br><br>
             총 다섯 팀이 열심히 준비한 무대를 선보일 예정이니, 3월 8일에 합정 드림홀에서 만나요!<br><br>
-            <strong>세트리스트</strong><br>
+            <strong>· 세트리스트</strong><br>
             잔나비-투게더! Fall Out Boy-Centuries<br>
             실리카겔 - Tik Tak Tok My Chemical Romance- Welcome to the Black Parade<br><br>
-            <strong>공연 문의 안내</strong><br>
+            <strong>· 공연 문의 안내</strong><br>
             공연에 대한 모든 문의는 <a href="https://link.to/kakao_channel" target="_blank">고스락 카카오톡 채널</a>로 남겨주세요!<br>
         `,
         refund_inf: `
-            <strong>환불 안내</strong><br>
+            <strong>· 환불 안내</strong><br>
             공연이라는 상품의 특성상 공연이 종료되면 상품 가치가 소멸합니다. 따라서 공연 시작 이후에는 환불이 어렵습니다. 공연 시작 시간은 각 공연 상세 페이지에서 확인하실 수 있습니다. 취소 수수료는 따로 없습니다.<br><br>
-            승인, 선착순 방식의 경우 <span style="color: #FF0000;">공연 시작 전까지</span> 마이페이지 예매내역에서 환불 가능합니다.<br><br>
-            <strong>공연 입장 안내</strong><br>
+            승인, 선착순 방식의 경우 <span style="color: #F01569;">공연 시작 전까지</span> 마이페이지 예매내역에서 환불 가능합니다.<br><br>
+            <strong>· 공연 입장 안내</strong><br>
             공연 입장 전에 "이름"과 "전화번호"를 말씀하시면, 공연자가 예매자 확인 후 입장이 가능합니다.<br>
         `
     },
     {
         id: 3,
         poster: poster4,
+        price: 4000,
         name: "[홍익대학교 고스락] 고스락 제 25회 정기공연",
         date: new Date('2024-08-18T14:00:00'),
         like: true,
         place: '합정 드림홀',
+        amount: 70,
         runningtime: 120,
         host: '고스락',
         cancel: new Date('2024-08-17T23:30:00'),
-        concert_inf: `
-            3월 7일, 활동 기수들의 멋진 공연에 이어, <br><br>
+        concert_inf:`
+            3월 7일, 활동 기수들의 멋진 공연에 이어, <br>
             이번에는 3월 8일 19시, 합정 드림홀에서 고스락 선배님들의 공연이 막을 올립니다!<br><br>
             총 다섯 팀이 열심히 준비한 무대를 선보일 예정이니, 3월 8일에 합정 드림홀에서 만나요!<br><br>
-            <strong>세트리스트</strong><br>
+            <strong>· 세트리스트</strong><br>
             잔나비-투게더! Fall Out Boy-Centuries<br>
             실리카겔 - Tik Tak Tok My Chemical Romance- Welcome to the Black Parade<br><br>
-            <strong>공연 문의 안내</strong><br>
+            <strong>· 공연 문의 안내</strong><br>
             공연에 대한 모든 문의는 <a href="https://link.to/kakao_channel" target="_blank">고스락 카카오톡 채널</a>로 남겨주세요!<br>
         `,
         refund_inf: `
-            <strong>환불 안내</strong><br>
+            <strong>· 환불 안내</strong><br>
             공연이라는 상품의 특성상 공연이 종료되면 상품 가치가 소멸합니다. 따라서 공연 시작 이후에는 환불이 어렵습니다. 공연 시작 시간은 각 공연 상세 페이지에서 확인하실 수 있습니다. 취소 수수료는 따로 없습니다.<br><br>
-            승인, 선착순 방식의 경우 <span style="color: #FF0000;">공연 시작 전까지</span> 마이페이지 예매내역에서 환불 가능합니다.<br><br>
-            <strong>공연 입장 안내</strong><br>
+            승인, 선착순 방식의 경우 <span style="color: #F01569;">공연 시작 전까지</span> 마이페이지 예매내역에서 환불 가능합니다.<br><br>
+            <strong>· 공연 입장 안내</strong><br>
             공연 입장 전에 "이름"과 "전화번호"를 말씀하시면, 공연자가 예매자 확인 후 입장이 가능합니다.<br>
         `
     },
     {
         id: 4,
         poster: poster1,
+        price: 4000,
         name: "[최장순&미지니 콘서트] 파란약방.exe",
         date: new Date('2024-08-03T14:00:00'),
         like: false,
         place: '합정 드림홀',
+        amount: 70,
         runningtime: 120,
         host: '고스락',
         cancel: new Date('2024-08-03T23:30:00'),
         concert_inf: `
-            3월 7일, 활동 기수들의 멋진 공연에 이어, <br><br>
+            3월 7일, 활동 기수들의 멋진 공연에 이어, <br>
             이번에는 3월 8일 19시, 합정 드림홀에서 고스락 선배님들의 공연이 막을 올립니다!<br><br>
             총 다섯 팀이 열심히 준비한 무대를 선보일 예정이니, 3월 8일에 합정 드림홀에서 만나요!<br><br>
-            <strong>세트리스트</strong><br>
+            <strong>· 세트리스트</strong><br>
             잔나비-투게더! Fall Out Boy-Centuries<br>
             실리카겔 - Tik Tak Tok My Chemical Romance- Welcome to the Black Parade<br><br>
-            <strong>공연 문의 안내</strong><br>
+            <strong>· 공연 문의 안내</strong><br>
             공연에 대한 모든 문의는 <a href="https://link.to/kakao_channel" target="_blank">고스락 카카오톡 채널</a>로 남겨주세요!<br>
         `,
         refund_inf: `
-            <strong>환불 안내</strong><br>
+            <strong>· 환불 안내</strong><br>
             공연이라는 상품의 특성상 공연이 종료되면 상품 가치가 소멸합니다. 따라서 공연 시작 이후에는 환불이 어렵습니다. 공연 시작 시간은 각 공연 상세 페이지에서 확인하실 수 있습니다. 취소 수수료는 따로 없습니다.<br><br>
-            승인, 선착순 방식의 경우 <span style="color: #FF0000;">공연 시작 전까지</span> 마이페이지 예매내역에서 환불 가능합니다.<br><br>
-            <strong>공연 입장 안내</strong><br>
+            승인, 선착순 방식의 경우 <span style="color: #F01569;">공연 시작 전까지</span> 마이페이지 예매내역에서 환불 가능합니다.<br><br>
+            <strong>· 공연 입장 안내</strong><br>
             공연 입장 전에 "이름"과 "전화번호"를 말씀하시면, 공연자가 예매자 확인 후 입장이 가능합니다.<br>
         `
     },
     {
         id: 5,
         poster: poster2,
+        price: 4000,
         name: "2024 시나 쓰는 앨리스 소극장 콘서트 [핑]",
+        amount: 70,
         date: new Date('2024-08-02T14:00:00'),
         like: false,
         place: '합정 드림홀',
@@ -155,26 +167,28 @@ export default Mockdata=[
         host: '고스락',
         cancel: new Date('2024-08-02T23:30:00'),
         concert_inf: `
-            3월 7일, 활동 기수들의 멋진 공연에 이어, <br><br>
+            3월 7일, 활동 기수들의 멋진 공연에 이어, <br>
             이번에는 3월 8일 19시, 합정 드림홀에서 고스락 선배님들의 공연이 막을 올립니다!<br><br>
             총 다섯 팀이 열심히 준비한 무대를 선보일 예정이니, 3월 8일에 합정 드림홀에서 만나요!<br><br>
-            <strong>세트리스트</strong><br>
+            <strong>· 세트리스트</strong><br>
             잔나비-투게더! Fall Out Boy-Centuries<br>
             실리카겔 - Tik Tak Tok My Chemical Romance- Welcome to the Black Parade<br><br>
-            <strong>공연 문의 안내</strong><br>
+            <strong>· 공연 문의 안내</strong><br>
             공연에 대한 모든 문의는 <a href="https://link.to/kakao_channel" target="_blank">고스락 카카오톡 채널</a>로 남겨주세요!<br>
         `,
         refund_inf: `
-            <strong>환불 안내</strong><br>
+            <strong>· 환불 안내</strong><br>
             공연이라는 상품의 특성상 공연이 종료되면 상품 가치가 소멸합니다. 따라서 공연 시작 이후에는 환불이 어렵습니다. 공연 시작 시간은 각 공연 상세 페이지에서 확인하실 수 있습니다. 취소 수수료는 따로 없습니다.<br><br>
-            승인, 선착순 방식의 경우 <span style="color: #FF0000;">공연 시작 전까지</span> 마이페이지 예매내역에서 환불 가능합니다.<br><br>
-            <strong>공연 입장 안내</strong><br>
+            승인, 선착순 방식의 경우 <span style="color: #F01569;">공연 시작 전까지</span> 마이페이지 예매내역에서 환불 가능합니다.<br><br>
+            <strong>· 공연 입장 안내</strong><br>
             공연 입장 전에 "이름"과 "전화번호"를 말씀하시면, 공연자가 예매자 확인 후 입장이 가능합니다.<br>
         `
     },
     {
         id: 6,
         poster: poster3,
+        price: 4000,
+        amount: 70,
         name: "[홍익대학교 소리얼] SO:RE-ALIVE",
         date: new Date('2024-08-01T14:00:00'),
         like: false,
@@ -182,50 +196,54 @@ export default Mockdata=[
         runningtime: 120,
         host: '고스락',
         cancel: new Date('2024-08-17T23:30:00'),
-        concert_inf: `
-            3월 7일, 활동 기수들의 멋진 공연에 이어, <br><br>
+        concert_inf:`
+            3월 7일, 활동 기수들의 멋진 공연에 이어, <br>
             이번에는 3월 8일 19시, 합정 드림홀에서 고스락 선배님들의 공연이 막을 올립니다!<br><br>
             총 다섯 팀이 열심히 준비한 무대를 선보일 예정이니, 3월 8일에 합정 드림홀에서 만나요!<br><br>
-            <strong>세트리스트</strong><br>
+            <strong>· 세트리스트</strong><br>
             잔나비-투게더! Fall Out Boy-Centuries<br>
             실리카겔 - Tik Tak Tok My Chemical Romance- Welcome to the Black Parade<br><br>
-            <strong>공연 문의 안내</strong><br>
+            <strong>· 공연 문의 안내</strong><br>
             공연에 대한 모든 문의는 <a href="https://link.to/kakao_channel" target="_blank">고스락 카카오톡 채널</a>로 남겨주세요!<br>
         `,
         refund_inf: `
-            <strong>환불 안내</strong><br>
+            <strong>· 환불 안내</strong><br>
             공연이라는 상품의 특성상 공연이 종료되면 상품 가치가 소멸합니다. 따라서 공연 시작 이후에는 환불이 어렵습니다. 공연 시작 시간은 각 공연 상세 페이지에서 확인하실 수 있습니다. 취소 수수료는 따로 없습니다.<br><br>
-            승인, 선착순 방식의 경우 <span style="color: #FF0000;">공연 시작 전까지</span> 마이페이지 예매내역에서 환불 가능합니다.<br><br>
-            <strong>공연 입장 안내</strong><br>
+            승인, 선착순 방식의 경우 <span style="color: #F01569;">공연 시작 전까지</span> 마이페이지 예매내역에서 환불 가능합니다.<br><br>
+            <strong>· 공연 입장 안내</strong><br>
             공연 입장 전에 "이름"과 "전화번호"를 말씀하시면, 공연자가 예매자 확인 후 입장이 가능합니다.<br>
         `
     },
     {
         id: 7,
         poster: poster4,
+        price: 4000,
         name: "[홍익대학교 고스락] 고스락 제 25회 정기공연",
         date: new Date('2024-08-01T14:00:00'),
+        amount: 70,
         like: false,
         place: '합정 드림홀',
         runningtime: 120,
         host: '고스락',
         cancel: new Date('2024-08-17T23:30:00'),
         concert_inf: `
-            3월 7일, 활동 기수들의 멋진 공연에 이어, <br><br>
+            3월 7일, 활동 기수들의 멋진 공연에 이어, <br>
             이번에는 3월 8일 19시, 합정 드림홀에서 고스락 선배님들의 공연이 막을 올립니다!<br><br>
             총 다섯 팀이 열심히 준비한 무대를 선보일 예정이니, 3월 8일에 합정 드림홀에서 만나요!<br><br>
-            <strong>세트리스트</strong><br>
+            <strong>· 세트리스트</strong><br>
             잔나비-투게더! Fall Out Boy-Centuries<br>
             실리카겔 - Tik Tak Tok My Chemical Romance- Welcome to the Black Parade<br><br>
-            <strong>공연 문의 안내</strong><br>
+            <strong>· 공연 문의 안내</strong><br>
             공연에 대한 모든 문의는 <a href="https://link.to/kakao_channel" target="_blank">고스락 카카오톡 채널</a>로 남겨주세요!<br>
         `,
         refund_inf: `
-            <strong>환불 안내</strong><br>
+            <strong>· 환불 안내</strong><br>
             공연이라는 상품의 특성상 공연이 종료되면 상품 가치가 소멸합니다. 따라서 공연 시작 이후에는 환불이 어렵습니다. 공연 시작 시간은 각 공연 상세 페이지에서 확인하실 수 있습니다. 취소 수수료는 따로 없습니다.<br><br>
-            승인, 선착순 방식의 경우 <span style="color: #FF0000;">공연 시작 전까지</span> 마이페이지 예매내역에서 환불 가능합니다.<br><br>
-            <strong>공연 입장 안내</strong><br>
+            승인, 선착순 방식의 경우 <span style="color: #F01569;">공연 시작 전까지</span> 마이페이지 예매내역에서 환불 가능합니다.<br><br>
+            <strong>· 공연 입장 안내</strong><br>
             공연 입장 전에 "이름"과 "전화번호"를 말씀하시면, 공연자가 예매자 확인 후 입장이 가능합니다.<br>
         `
     },
 ]
+
+export default Mockdata;
--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -4,57 +4,86 @@ import { useState } from "react";
 import Navbar_Booking from "../components/common/Navbar_Booking";
 import Footer from "../components/common/Footer";
 import Book_component from "../components/booking/Book_component";
-import mockdata from "../components/booking/mockdata";
+import Mockdata from "../components/booking/Mockdata";
 
 import Star1 from "../assets/img_Ready/Star1.svg";
 import Star2 from "../assets/img_Ready/Star2.svg";
 import search from "../assets/img_Booking/Booking/search.svg";
 
+import Book_detail from "../components/booking/Book_detail";
+
 const Booking = () => {
     const [searchTerm, setSearchTerm] = useState(""); // 검색어 상태
-    const [filteredConcerts, setFilteredConcerts] = useState(mockdata); // 필터링된 결과 상태
+    const [filteredConcerts, setFilteredConcerts] = useState(Mockdata); // 필터링된 결과 상태
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedConcert, setSelectedConcert] = useState(null); // 선택된 공연 상태
 
     const handleSearch = () => {
-        const filtered = mockdata.filter(concert =>
+        const filtered = Mockdata.filter(concert =>
             concert.name.toLowerCase().includes(searchTerm.toLowerCase())
         );
         setFilteredConcerts(filtered);
+    };
+
+    const handleClick = (concert) => {
+        setSelectedConcert(concert); // 선택된 공연 데이터를 저장
+        setIsOpen(true); // 상세 설명 화면 열기
     };
 
     return (
         <div className="Booking">
             <Navbar_Booking />
             <Footer />
-            <div className="Booking_container">
-                <div className="Booking_Header">
-                    <img className="Star1" src={Star1} alt="Star" />
-                    <h2><span className="highlight">ShowHoo</span>에서 다양한 <br />공연들을 만나보세요</h2>
-                    <img className="Star2" src={Star2} alt="Star" />
-                </div>
-                <div className="Booking_search">
-                    <input
-                        placeholder="공연 검색"
-                        value={searchTerm}
-                        onChange={(e) => setSearchTerm(e.target.value)}
-                    />
-                    <div className="img" onClick={handleSearch}>
-                        <img className="search" src={search} alt="search" />
+            {!isOpen && (
+                <div className="Booking_container">
+                    <div className="Booking_Header">
+                        <img className="Star1" src={Star1} alt="Star" />
+                        <h2><span className="highlight">ShowHoo</span>에서 다양한 <br />공연들을 만나보세요</h2>
+                        <img className="Star2" src={Star2} alt="Star" />
+                    </div>
+                    <div className="Booking_search">
+                        <input
+                            placeholder="공연 검색"
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                        />
+                        <div className="img" onClick={handleSearch}>
+                            <img className="search" src={search} alt="search" />
+                        </div>
+                    </div>
+                    <div className="Booking_content">
+                        {filteredConcerts.map((concert) => (
+                            <Book_component
+                                key={concert.id}
+                                id={concert.id}
+                                img={concert.poster}
+                                name={concert.name}
+                                date={concert.date}
+                                onClick={() => handleClick(concert)} // 클릭 핸들러 추가
+                            />
+                        ))}
                     </div>
                 </div>
-                <div className="Booking_content">
-                    {filteredConcerts.map((concert) => (
-                        <Book_component
-                            key={concert.id}
-                            id={concert.id}
-                            img={concert.poster}
-                            name={concert.name}
-                            date={concert.date}
-                        />
-                    ))}
-                </div>
-            </div>
+            )}
+            {isOpen && selectedConcert && (
+                <Book_detail
+                    key={selectedConcert.id}
+                    id={selectedConcert.id}
+                    name={selectedConcert.name}
+                    img={selectedConcert.poster}
+                    place={selectedConcert.place}
+                    date={selectedConcert.date}
+                    runningtime={selectedConcert.runningtime}
+                    host={selectedConcert.host}
+                    cancel={selectedConcert.cancel}
+                    concert_inf={selectedConcert.concert_inf}
+                    sell_inf={selectedConcert.refund_inf}
+                    price={selectedConcert.price}
+                    amount={selectedConcert.amount}
+                />
+            )}
         </div>
     );
-}
+};
 
 export default Booking;
