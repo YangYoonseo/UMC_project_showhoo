f6cc38f1aa13b7470964f6598de96bbab28f07b5
Jisu <92006284+jsomnium@users.noreply.github.com>
2024-08-10 15:32:26 +0900

    Reapply "Feat: 검색 바 추가"
    This reverts commit ca53e82f93b8b0dad2919f5601b9bc5f771ea786.

---
+++ src/pages/_test_/BookingAfter.jsx
@@ -0,0 +1,9 @@
+const BookingAfter = () => {
+    return (
+      <div className="BookingAfter">
+        <h2>BookingAfter Page@</h2>
+      </div>
+    );
+  };
+
+export default BookingAfter;
--- src/pages/_test_/Home_seoya.jsx
+++ src/pages/_test_/Home_seoya.jsx
@@ -3,7 +3,7 @@ import HomeImage from "../../components/_test_/Home/HomeImage";
 import MainInfo from "../../components/_test_/Home/MainInfo";
 import Footer from "../../components/common/Footer";
 
-// 임시 Home Page
+// 임시 Home Page 이미지
 const Home = () => {
   return (
     <div className="Home">
