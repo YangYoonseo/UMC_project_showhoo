48ed7473b2b566245f7b4243224884aa16e5b887
Jisu <92006284+jsomnium@users.noreply.github.com>
2024-08-10 23:44:13 +0900

    Feat: 대관 상세페이지 레이아웃 설정

--- src/pages/_test_/RentalSearch.jsx
+++ src/pages/_test_/RentalSearch.jsx
@@ -1,7 +1,31 @@
+import "../../styles/Jisu/rentalSearch.css";
+
+import Navbar_Perforemr from "../../components/common/Navbar_Performer";
+import BookingSearchBar from "../../components/_test_/Booking/BookingSearchBar";
+import RentalSearchFilter from "../../components/rental/RentalSearchFilter.jsx"
+
+// HOT 공연장 리스트 가져오기
+
 const RentalSearch = () => {
-    return (
-      <div className="RentalSearch">
-        <h2>RentalSearch Page@</h2>
+  return (
+      <div className="rentalSearchBody">
+        <Navbar_Perforemr />
+        <div className="gap130"></div>
+      
+        <div className="BarAndFilter">
+          <BookingSearchBar />
+          <RentalSearchFilter />
+      </div>
+      
+      <div className="gap58"></div>
+      <div className="ConcertHallAndMap">
+        <div className="ConcertHall">
+
+        </div>
+        <div className="Map">
+          
+        </div>
+      </div>
       </div>
     );
   };
