054701d81d3aa14135ad3cfddbd6e54af851ae7e
Jisu <92006284+jsomnium@users.noreply.github.com>
2024-08-14 18:06:34 +0900

    style: home, rental 페이지 css 조정

--- src/components/_test_/Booking/BookingInfoPoster.jsx
+++ src/components/_test_/Booking/BookingInfoPoster.jsx
@@ -1,12 +1,11 @@
-import info_poster from "../../../assets/img_Booking/info_poster.svg"
+import info_poster from "../../../assets/img_Booking/info_poster.png"
 
 // 공연자 - 공연장 대관 - 공연장 정보 제공
 // 화면에 뜨는 공연 포스터 컴포넌트입니다.
 
 const BookingInfoPoster = () => {
-    return (<div className="BookingPoster">
+    return (
         <img src={info_poster} alt="info_poster" />
-    </div>
     );
 };
 
