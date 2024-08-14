7a05c2969fe94211a0df0b9694f121d6ddbb69b1
Jisu <92006284+jsomnium@users.noreply.github.com>
2024-08-14 17:44:30 +0900

    refactor: HotConcertHall 가독성 좋게 리팩토링

--- src/components/_test_/Booking/HotConcertHall.jsx
+++ src/components/_test_/Booking/HotConcertHall.jsx
@@ -1,70 +1,54 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
-import "../../../styles/Jisu/HotConcertHall.css"
+import "../../../styles/Jisu/HotConcertHall.css";
 import LikeButton from "./LikeButton";
-import PeopleIcon from "./../../../assets/img_Booking/people_icon.svg"
-import AreaIcon from "./../../../assets/img_Booking/area_icon.svg"
-import DetailIcon from "./../../../assets/img_Booking/detail_icon.svg"
-
-import StarPointImg from '../../../assets/img_Booking/vector19.svg'
+import PeopleIcon from "./../../../assets/img_Booking/people_icon.svg";
+import AreaIcon from "./../../../assets/img_Booking/area_icon.svg";
+import DetailIcon from "./../../../assets/img_Booking/detail_icon.svg";
+import StarPointImg from '../../../assets/img_Booking/vector19.svg';
 
 const HotConcertHall = () => {
-    const nav = useNavigate();
-        
-    return <div className="HotConcertHall">
+    return (
+        <div className="HotConcertHall">
             <div className="LikeAndImg">
-            <img
-                className="ConcertHallImg"
-                src="src/assets/img_Booking/_test_/concert_hall_2.jpg"
-                alt="Concert Hall"
-            />
-            <div className="LikeButton">
-                <LikeButton className="LikeButtonBody" />
+                <img
+                    className="ConcertHallImg"
+                    src="src/assets/img_Booking/_test_/concert_hall_2.jpg"
+                    alt="Concert Hall"
+                />
+                <div className="LikeButton">
+                    <LikeButton />
+                </div>
+            </div>
+            <div className="ConcertHallBody">
+                <div className="TopInfo">
+                    <div className="TopInfoText">
+                        <div className="Name">리엠아트센터</div>
+                        <div className="Location">서울 강남구 학동로 2길 56</div>
+                    </div>
+                    <div className="StarRate">
+                        <img src={StarPointImg} alt="Star Rating" />
+                        <span className="StarRatePoint">4.7</span>
+                    </div>
+                </div>
+                <div className="BottomInfo">
+                    <div className="InfoItem">
+                        <img className="Icon" src={PeopleIcon} alt="People Icon" />
+                        <span className="InfoText">최대 300명</span>
+                    </div>
+                    <div className="InfoItem">
+                        <img className="Icon" src={AreaIcon} alt="Area Icon" />
+                        <span className="InfoText">661m2</span>
+                    </div>
+                    <div className="InfoItem">
+                        <img className="Icon" src={DetailIcon} alt="Detail Icon" />
+                        <span className="InfoText">공연 영상 제공</span>
+                    </div>
+                </div>
+                <div className="Price">
+                    ₩200,000~
+                </div>
             </div>
         </div>
-        
-        <div className="Info">
-            <div className="TopInfo">
-                <div className="TopInfoText">
-                    <div className="Name">리엠아트센터</div>
-                    <div className="Location">서울 강남구 학동로 2길 56</div>
-                </div>
-                <div className="StarRate">
-                    <img src={StarPointImg} alt="StarRateImg" />
-                    <div className='StarRatePoint'>4.7</div>
-                </div>
-            </div>
-            <div className="BottomInfo">
-                <div className="People">
-                    <div className="Icon">
-                        <img src={PeopleIcon} alt="PeopleIcon" />
-                    </div>
-                    <div>
-                        최대 300명
-                    </div>
-                </div>
-                <div className="Size">
-                    <div className="Icon">
-                        <img src={AreaIcon} alt="AreaIcon" />
-                    </div>
-                    <div>
-                        661m2
-                    </div>
-                </div>
-                <div className="Detail">
-                    <div className="Icon">
-                        <img src={DetailIcon} alt="DetailIcon" />
-                    </div>
-                    <div>
-                        공연영상 제공
-                    </div></div>
-            </div>
-            <div className="Price">
-                ₩200,000~
-            </div>
-        </div>
-    </div>
+    );
 };
 
 export default HotConcertHall;
