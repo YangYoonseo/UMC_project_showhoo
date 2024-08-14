47aee00342216b869d30c01612e1fa3aea8131bb
Jisu <92006284+jsomnium@users.noreply.github.com>
2024-08-14 18:06:34 +0900

    style: home, rental 페이지 css 조정

--- src/components/_test_/Booking/LikeButton.jsx
+++ src/components/_test_/Booking/LikeButton.jsx
@@ -1,29 +1,20 @@
 import unlikeImg from "../../../assets/img_Booking/unlike.svg";
 import likeImg from "../../../assets/img_Booking/like.svg";
 import React, { useState } from "react";
+import "../../../styles/Jisu/LikeButton.css"; // CSS 파일 import
 
 function LikeButton() {
     const [liked, setLiked] = useState(false);
 
     return (
-            <button
-                style={{
-                    width: "31.27px",
-                    height: "31.27px",
-                    borderRadius: "50%",
-                    border: "none",
-                    backgroundColor: "white",
-                    display: "flex", // Flexbox를 사용하여 자식 요소를 가운데 정렬
-                    alignItems: "center", // 수직 가운데 정렬
-                    justifyContent: "center", // 수평 가운데 정렬
-                    position: "absolute"
-                }}
-                onClick={() => {
-                    setLiked(!liked);
-                }}
-            >
-                <img src={liked ? likeImg : unlikeImg} alt="like-button-icon" />
-            </button>
+        <button
+            className="like-button" // CSS 클래스 적용
+            onClick={() => {
+                setLiked(!liked);
+            }}
+        >
+            <img src={liked ? likeImg : unlikeImg} alt="like-button-icon" />
+        </button>
     );
 }
 
