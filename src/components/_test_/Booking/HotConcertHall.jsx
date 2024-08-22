64904fc41795c7709231e31f0c98c489ee63bf73
Jisu <92006284+jsomnium@users.noreply.github.com>
2024-08-22 18:06:12 +0900

    Feat: 공연장 클릭시 대관 페이지로 이동하도록

--- src/components/_test_/Booking/HotConcertHall.jsx
+++ src/components/_test_/Booking/HotConcertHall.jsx
@@ -1,4 +1,5 @@
 import "../../../styles/Jisu/HotConcertHall.css";
+import { useNavigate } from "react-router-dom";
 import LikeButton from "./LikeButton";
 import PeopleIcon from "./../../../assets/img_Booking/people_icon.svg";
 import AreaIcon from "./../../../assets/img_Booking/area_icon.svg";
@@ -7,6 +8,12 @@ import StarPointImg from '../../../assets/img_Booking/vector19.svg';
 import defaultHallImg from "../../../../src/assets/img_Booking/_test_/concert_hall_2.jpg"
 
 const HotConcertHall = ({ hall }) => {
+    const navigate = useNavigate(); // useNavigate 훅 사용
+
+    const handleImageClick = () => {
+        navigate(`/venue_detail/${hall.spaceId}`); // 클릭 시 해당 경로로 이동
+    };
+
     return (
         <div className="HotConcertHall">
             <div className="LikeAndImg">
@@ -14,6 +21,8 @@ const HotConcertHall = ({ hall }) => {
                     className="ConcertHallImg"
                     src={hall.imageURL || defaultHallImg}  // 기본 이미지 제공
                     alt={hall.name || "Concert Hall"}
+                    onClick={handleImageClick} // 이미지 클릭 이벤트 핸들러
+                    style={{ cursor: "pointer" }} // 클릭 가능한 커서 스타일 추가
                 />
                 <div className="LikeButton">
                     <LikeButton spaceId={hall.spaceId} performerId={1} />
@@ -22,7 +31,10 @@ const HotConcertHall = ({ hall }) => {
             <div className="ConcertHallBody">
                 <div className="TopInfo">
                     <div className="TopInfoText">
-                        <div className="Name">{hall.name}</div>
+                        <div className="Name"
+                            onClick={handleImageClick} // 이미지 클릭 이벤트 핸들러
+                            style={{ cursor: "pointer" }} // 클릭 가능한 커서 스타일 추가
+                        >{hall.name}</div>
                         <div className="Location">{hall.location}</div>
                     </div>
                     <div className="StarRate">
