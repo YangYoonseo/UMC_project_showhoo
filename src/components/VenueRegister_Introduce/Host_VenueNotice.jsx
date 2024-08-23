92d296157a1366aad2ef964c3a4cd5f97985527d
ydjwls <xqhfkfpalx@naver.com>
2024-08-23 13:55:12 +0900

    공연장 등록 페이지 완료

--- src/components/VenueRegister_Introduce/Host_VenueNotice.jsx
+++ src/components/VenueRegister_Introduce/Host_VenueNotice.jsx
@@ -2,7 +2,7 @@ import "../../styles/Eojin/Host_VenueNotice.css";
 import { useState, useEffect } from "react";
 import Editor from "../perform_Ready/Editor/Editor";
 
-const Host_VenueNotice = ({ updateNotice, notice }) => {
+const Host_VenueNotice = ({ updateNotice }) => {
     const [getNotice, setGetNotice] = useState('');
 
     const handleNotice = (notice) => {
@@ -17,11 +17,8 @@ const Host_VenueNotice = ({ updateNotice, notice }) => {
         <div className="Host_VenueNotice">
             <h4>유의사항</h4>
             <p>환불규정, 대관료, 예약금, 잔금에 대한 주의사항 등 공연장을 대관할 시 공연자가 꼭 알아야 할 것들을 입력해주세요</p>
-            <div className="editor" style={{ display: notice === '' ? "block" : "none" }}>
+            <div className="editor">
                 <Editor onContentChange={handleNotice} />
-            </div>
-            <div className="notice-section" style={{ display: notice !== '' ? "block" : "none" }}>
-                <div dangerouslySetInnerHTML={{ __html: notice }} />
             </div>
         </div>
     )
